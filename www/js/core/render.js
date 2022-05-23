8cea8a52ab5c10a8fc6d581d62617e4f59542b16
azern32 <chiro.emiya@gmail.com>
2022-05-23 15:08:31 +0800

    plan

---
+++ www/js/core/render.js
@@ -0,0 +1,11 @@
+const renderer = new PIXI.Renderer({
+    antialias:true,
+    // width:window.screen.availWidth,
+    // height:window.screen.availHeight,
+    width:720,
+    height:400,
+    backgroundColor:0xeeeeee,
+})
+
+
+var camera = new PIXI.Container()
--- www/js/orientation.js
+++ www/js/orientation.js
@@ -1,4 +1,4 @@
-    window.screen.orientation.lock('landscape-primary');
+window.screen.orientation.lock('landscape-primary');
 
 window.screen.orientation.unlock();
 
